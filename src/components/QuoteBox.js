ba6bac830ba5dc15d84be374966cd8aef0ec2273
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-11-30 00:12:28 +0530

    First commit

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/base.css';
+
+function QuoteBox() {
+  return (
+    <div className='text-center quoteBox'>
+      <h1>Hello World!</h1>
+      <p>This is React</p>
+    </div>
+  )
+}
+
+export default QuoteBox;
---
+++ src/containers/AppContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Hello from '../components/QuoteBox';
+
+class AppContainer extends Component{
+  render() {
+    return (
+      <Hello />
+    )
+  }
+}
+
+export default AppContainer;
