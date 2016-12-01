90f9c8bad8607a1db82f905e0577bfb03690b69a
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-12-01 18:15:14 +0530

    Added the tweeting functionality.

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,14 +1,19 @@
 import React from 'react';
+
 import '../styles/base.css';
 
 function QuoteBox(props) {
+  let tweetText = "'" + props.quote + "' - " + props.author;
+  let tweetLink = 'https://twitter.com/intent/tweet?text=' + tweetText;
   return (
     <div className='container-fluid quoteBox'>
       <blockquote className='blockquote'>
-      <p className='mb-0'> {props.quote} </p>
-      <footer className='blockquote-footer'> {props.author} </footer>
+        <p className='mb-0'>{props.quote}</p>
+        <footer className='blockquote-footer'>{props.author}</footer>
       </blockquote>
-      <button className='btn btn-default pull-left' type='submit'>Tweet</button>
+      <a className='btn btn-default pull-left' href={tweetLink} type='submit'>
+        <i className='fa fa-twitter'> Tweet</i>
+      </a>
       <button className='btn btn-default pull-right' type='submit'>New Quote</button>
     </div>
   )
