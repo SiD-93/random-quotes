a575b46b54efbbf038e7f5993e0bf9001d37512a
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-12-01 13:11:40 +0530

    Adding QuoteBox styles and state.

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../styles/base.css';
 
-function QuoteBox() {
+function QuoteBox(props) {
   return (
-    <div className='text-center quoteBox'>
-      <h1>Hello World!</h1>
-      <p>This is React</p>
+    <div className='container-fluid quoteBox'>
+      <blockquote className='blockquote'>
+      <p className='mb-0'> {props.quote} </p>
+      <footer className='blockquote-footer'> {props.author} </footer>
+      </blockquote>
+      <button className='btn btn-default pull-left' type='submit'>Tweet</button>
+      <button className='btn btn-default pull-right' type='submit'>New Quote</button>
     </div>
   )
+}
+
+QuoteBox.propTypes = {
+  quote: React.PropTypes.string.isRequired,
+  author: React.PropTypes.string.isRequired
 }
 
 export default QuoteBox;
